7a4005e738257d8360080fed83920fc03fd3b9cc
Yves Schubert <yves.schubert@posteo.de>
2017-01-21 23:10:07 +0100

    First commit

---
+++ both/routes.js
@@ -0,0 +1,39 @@
+import { Meteor } from 'meteor/meteor'
+
+Router.configure({
+    layoutTemplate: 'layout',
+});
+
+Router.route('/', function () {
+    if (Meteor.userId()) {
+        this.render('overview');
+    } else {
+        this.render('login');
+    }
+}, { name: 'overview' });
+
+Router.route('/login', function () {
+    this.render('login');
+}, { name: 'login' });
+
+/*
+Router.route('/project/:projectId', function () {
+    const projectId = this.params.projectId;
+    Meteor.subscribe("projects", projectId);
+    Session.set('selectedProject', projectId);
+    this.render('sprints');
+}, { name: 'sprints' });
+
+Router.route('/sprint/:sprintId', function () {
+    const sprintId = this.params.sprintId;
+    Meteor.subscribe("singleSprint", sprintId, () => {
+        const projectId = Sprints.findOne().projectId;
+        Meteor.subscribe("projects", projectId, () => {
+            Session.set('selectedProject', projectId);
+        });
+    });
+    Session.set('selectedSprint', sprintId);
+    this.render('availabilities');
+}, { name: 'availabilities' });
+
+    */
